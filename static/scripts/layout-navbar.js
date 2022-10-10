7516fcc413eb13e412d436e604c1a3626f520b29
David Toh <davidlabyrinth@gmail.com>
2022-10-10 12:59:17 +0800

    Shrinking header

--- static/scripts/layout-navbar.js
+++ static/scripts/layout-navbar.js
@@ -9,3 +9,15 @@ $(document).ready(function() {
     });
     
 });
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+        console.log("Displaying tiny header");
+        $(".header__icon").addClass("header__icon--tiny");
+        $(".header__title").addClass("header__title--tiny");
+    } else {
+        console.log("Displaying big header");
+        $(".header__icon").removeClass("header__icon--tiny");
+        $(".header__title").removeClass("header__title--tiny");
+    }
+}
